c07b4e9f178f64adcbdc8753f8509e1701a0392b
sakis.kaliakoudas <sakis.kaliakoudas@hotmail.com>
2021-04-13 09:13:53 +0100

    Small changes to reproduce the micronaut security issue

--- complete/src/main/kotlin/example/micronaut/BookController.kt
+++ complete/src/main/kotlin/example/micronaut/BookController.kt
@@ -1,14 +1,22 @@
 package example.micronaut
+
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.authentication.Authentication
+import io.micronaut.security.rules.SecurityRule
 import java.util.UUID
 import javax.validation.Valid
 
 @Controller // <1>
 open class BookController {
     @Post // <2>
-    open fun save(@Valid @Body book: Book): BookSaved {  // <3>
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    open fun save(
+        @Valid @Body book: Book,
+        authentication: Authentication
+    ): BookSaved {  // <3>
         val bookSaved = BookSaved()
         bookSaved.name = book.name
         bookSaved.isbn = UUID.randomUUID().toString()
